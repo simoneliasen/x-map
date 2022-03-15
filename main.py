489256874724d93ee17fa14daf89f474b46cacb5
Simon Eliasen <simoneliasen@gmail.com>
2022-03-15 19:36:19 +0100

    simple salient map example

--- main.py
+++ main.py
@@ -1,47 +1,102 @@
-#Defining convolutional neural network
-import torch.nn as nn
-#For defining loss function and optimizer
-import torch.optim as optim
-#import wandb
-from matplotlib.path import Path
-from matplotlib.patches import PathPatch
-#Imports from other local files
-from model import Net
-from test import testing
-from train import training
-from load_data import load_data
+import torch
+import torchvision
+import torchvision.transforms as transforms
+# Display images
+from PIL import Image
+#For saliency
+import numpy as np
+import matplotlib.pyplot as plt
 
-#Log in to wandb here
-#wandb.login()
+#load pretrained resnet model
+model = torchvision.models.alexnet(pretrained=True)
+print(model)
 
-#wandb.init(
-#    project="my-test-project", 
-#    entity="simoneliasen", 
-#    config={
-#        "learning_rate": 0.001,
-#        "momentum": 0.9,
-#        "epochs": 3
-#        }
-#    )
+#define transforms to preprocess input image into format expected by model
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+#inverse transform to get normalize image back to original form for visualization
+inv_normalize = transforms.Normalize(
+    mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
+    std=[1/0.229, 1/0.224, 1/0.255]
+)
 
-#config = wandb.config
-# Pass config.epochs to training, also wandb.log(loss)
+#transforms to resize image to the size expected by pretrained model,
+#convert PIL image to tensor, and
+#normalize the image
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    normalize,          
+])
 
-#load data
-transform, batch_size, trainset, trainloader, testset, testloader, classes = load_data()
 
-#Convolutional Neural network + Optimizer and Loss function
-net = Net()
-criterion = nn.CrossEntropyLoss() #Can this be defined within model? (check flatland)
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-#Path to save and load model
-PATH = './cifar_net.pth'
+############ SALIENCY MAPPING #################
 
-#Training
-training(trainloader, optimizer, net, criterion, PATH)
+def saliency(img, model):
+    #we don't need gradients w.r.t. weights for a trained model
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    #set model in eval mode
+    model.eval()
+    #transoform input PIL image to torch.Tensor and normalize
+    input = transform(img)
+    input.unsqueeze_(0)
 
-#Testing
-testing(testloader, classes, PATH)
+    #we want to calculate gradient of higest score w.r.t. input
+    #so set requires_grad to True for input 
+    input.requires_grad = True
+    #forward pass to calculate predictions
+    preds = model(input)
+    score, indices = torch.max(preds, 1)
+    #backward pass to get gradients of score predicted class w.r.t. input image
+    score.backward()
+    #get max along channel axis
+    slc, _ = torch.max(torch.abs(input.grad[0]), dim=0)
+    #normalize to [0..1]
+    slc = (slc - slc.min())/(slc.max()-slc.min())
 
-#wandb.finish()
+    #apply inverse transform on image
+    with torch.no_grad():
+        input_img = inv_normalize(input[0])
+    #plot image and its saleincy map
+    plt.figure(figsize=(10, 10))
+    plt.subplot(1, 2, 1)
+    plt.imshow(np.transpose(input_img.detach().numpy(), (1, 2, 0)))
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(1, 2, 2)
+    plt.imshow(slc.numpy(), cmap=plt.cm.hot)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+
+img = Image.open('images/monkey.jpg').convert('RGB')
+saliency(img, model)
+
+img = Image.open('images/puppy.jpg').convert('RGB')
+saliency(img, model)
+
+img = Image.open('images/space.jpg').convert('RGB')
+saliency(img, model)
+
+img = Image.open('images/aircraft.jpg').convert('RGB')
+saliency(img, model)
+
+
+
+img = Image.open('images/gorilla.jpg').convert('RGB')
+saliency(img, model)
+
+
+
+
+
+
+
+
+# Cam library something
+#https://github.com/zhoubolei/CAM
