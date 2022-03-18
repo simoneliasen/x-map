0be1251291a9cdd74f10f00cf56fc51bc67302de
Dennis <tran.dennis@hotmail.com>
2022-03-18 14:03:18 +0100

    preprocessing function ready

--- load_data_test.py
+++ load_data_test.py
@@ -3,12 +3,16 @@ from torchvision import transforms, datasets
 import pandas as pd
 
 batch_size=5
-dataset = datasets.ImageFolder(root = "/PP_data")
-train_loader = torch.utils.data.Dataloader(
-   dataset, transform=transforms.Compose([
-                      transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
-                      transforms.RandomHorizontalFlip(p=0.3),
-                      transforms.RandomAutocontrast(p=0.3),
-                      transforms.RandomAffine(degrees = (0,30), translate = (0.3,0,1)),
-                      transforms.ToTensor()
-                  ]), batch_size=batch_size, shuffle=True)
+dataset = datasets.ImageFolder(root = "PP_data")
+
+transform = { 'train': transforms.Compose([
+                    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+                    transforms.RandomHorizontalFlip(p=0.3),
+                    transforms.RandomAutocontrast(p=0.3),
+                    transforms.RandomAffine(degrees = (0,30), translate = (0.3,0.1)),
+                    transforms.ToTensor()]),
+                'test': transforms.Compose([
+                    ##
+                ])
+}
+
