7fc9d8a63efb884c420d9277dde9a5c0d56c6917
Dennis <tran.dennis@hotmail.com>
2022-03-20 15:53:02 +0100

    transformed + normalized data

--- load_data.py
+++ load_data.py
@@ -3,8 +3,7 @@ from torchvision import transforms, datasets
 import pandas as pd
 
 batch_size=5
-dataset = datasets.ImageFolder(root = "PP_data")
-
+normalized_dataset = datasets.ImageFolder(root = "PP_data")
 transform = { 'train': transforms.Compose([
                     transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
                     transforms.RandomHorizontalFlip(p=0.3),
@@ -12,7 +11,15 @@ transform = { 'train': transforms.Compose([
                     transforms.RandomAffine(degrees = (0,30), translate = (0.3,0.1)),
                     transforms.ToTensor()]),
                 'test': transforms.Compose([
-                    ##
+                    #
                 ])
 }
 
+transformed_dataset = datasets.ImageFolder(root = "PP_data", transform = transform)
+
+
+#classes with corresponding id. (tb_negative 0, tb_positive 1)
+print(transformed_dataset.class_to_idx)
+
+#Paths of all pictures and corresponding labels
+#print(transformed_dataset.imgs)
