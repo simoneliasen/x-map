723a645be54220040f68ea1586b68fd692e3c921
Dennis <tran.dennis@hotmail.com>
2022-03-24 00:30:10 +0100

    normalization restructure

--- load_data.py
+++ load_data.py
@@ -1,35 +1,74 @@
+from pkgutil import get_data
+from numpy import string_
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, ConcatDataset
 from torchvision import transforms, datasets
 import pandas as pd
+import shutil
+import os
 from sklearn.model_selection import KFold
+import time
 
-normalized_dataset = datasets.ImageFolder(root = "PP_data", transform = transforms.Compose([transforms.ToTensor()]))
+def label_files(img_dir):
+    mypath = 'PP_data'
+    for f in os.listdir(img_dir):
+        # move if TB positive
+        if f.endswith('1.png'):
+            shutil.move(os.path.join(img_dir, f), os.path.join(mypath,"TB_Positive"))
+        # move if TB negative
+        elif f.endswith('0.png'):
+            shutil.move(os.path.join(img_dir, f), os.path.join(mypath, "TB_Negative"))
+        # notify if something else
+        else:
+            print('Could not categorize file with name %s' % f)
 
-mean = img_tr.mean([1,2])
-transform = transforms.Compose([
-                    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
-                    transforms.RandomHorizontalFlip(p=0.3),
-                    transforms.RandomAutocontrast(p=0.3),
-                    transforms.RandomAffine(degrees = (0,30), translate = (0.3,0.1)),
-                    transforms.Grayscale(1),
-                    transforms.ToTensor()])
+
+def get_dataloader(img_dir, batch_size):
+    label_files(img_dir)
+
+    normalize = transforms.Compose([
+                        transforms.Resize(256),
+                        transforms.Grayscale(1),
+                        transforms.ToTensor()])
+
+    normalized_dataset = datasets.ImageFolder(root = "PP_data", transform = normalize)
+
+    transform = transforms.Compose([
+                        transforms.Resize(256),
+                        transforms.Grayscale(1),
+                        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+                        transforms.RandomHorizontalFlip(p=0.3),
+                        transforms.RandomAutocontrast(p=0.3),
+                        transforms.RandomAffine(degrees = (0,30), translate = (0.3,0.1)),
+                        transforms.ToTensor()])
 
 
 
-transformed_dataset = datasets.ImageFolder(root = "PP_data", transform = transform)
+    transformed_dataset = datasets.ImageFolder(root = "PP_data", transform = transform)
 
-concat_dataset = ConcatDataset([normalized_dataset, transformed_dataset])
+    concat_dataset = ConcatDataset([normalized_dataset, transformed_dataset])
+  
+    list_of_classes=list(map(str, list(transformed_dataset.classes)) )
 
-train_dataset = DataLoader(transformed_dataset, batch_size = 5, shuffle=True)
-#test_dataset = DataLoader(concat_dataset, batch_size = 5, shuffle=True)
+    print(list_of_classes)
+    for idx, (sample, target) in enumerate(concat_dataset):
+        print(sample, list_of_classes[target] )
 
-for i in range(1):
-    for x in normalized_dataset:
-        print(x)
+    dl_cds = DataLoader(concat_dataset, batch_size = batch_size, shuffle=True)
 
-# dataset = ConcatDataset([train_dataset, test_dataset])
+
+#Kims Path: 'C:/Users/Monkk/OneDrive/Dokumenter/AAU/CS/CS2/01.P8/Data/ChinaSet_AllFiles/CXR_png'
+#Dennis Stationær: D:/Downloads/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png
+#Dennis laptop: C:/Users/Dennis/Downloads/xrayTB/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png'
+
+img_dir ='D:/Downloads/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png'
+get_dataloader(img_dir, batch_size = 5)
+
+# iterate through entire dataset and count how many elements there are
+# count = 0
+# for x in normalized_dataset:
+#      print(x)
 
 # #k-fold cross validation
 
--- preprocess.py
+++ preprocess.py
@@ -2,19 +2,20 @@ import numpy as np
 from numpy import asarray
 #from skimage.io import imread
 import os
-from PIL import Image, ImageOps
+#from PIL import Image, ImageOps
 import time
 import shutil
+import cv2
 
-def label_files():
+def label_files(img_dir):
     mypath = 'PP_data'
-    for f in os.listdir(mypath):
+    for f in os.listdir(img_dir):
         # move if TB positive
         if f.endswith('1.png'):
-            shutil.move(os.path.join(mypath, f), os.path.join(mypath,"TB_Positive"))
+            shutil.move(os.path.join(img_dir, f), os.path.join(mypath,"TB_Positive"))
         # move if TB negative
         elif f.endswith('0.png'):
-            shutil.move(os.path.join(mypath, f), os.path.join(mypath, "TB_Negative"))
+            shutil.move(os.path.join(img_dir, f), os.path.join(mypath, "TB_Negative"))
         # notify if something else
         else:
             print('Could not categorize file with name %s' % f)
@@ -27,41 +28,32 @@ def preprocess(img_dir):
     for img_name in img_lst:
 
         #Resize image
-        img = Image.open(os.path.join(img_dir, img_name))
-        img = img.resize((256,256), Image.ANTIALIAS)
-
-        #Greyscale image
-        img = ImageOps.grayscale(img)
-       
+        img = cv2.imread(os.path.join(img_dir, img_name), cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (256, 256), interpolation = cv2.INTER_AREA)
+        before_img = asarray(img)
+        #pixels = pixels.astype('float32')
+        print("before normalization")        
+        print(before_img)
+        norm_img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        #cv2.imshow('hej', norm_img)
+        #cv2.waitKey(0)
+        cv2.imwrite('PP_Data/' + img_name, 255*norm_img)
         #Transform image to np.array
-        pixels = asarray(img)
-        pixels = pixels.astype('float32')
+        pixels = asarray(norm_img)
+        print("after normalization")
+        print(pixels)
         
-        #DEBUG: check how many channels there are, and which mode. "L" Greyscale, "P" Palet, "RGB" RGB.
-        #print(img.getbands())
-        #print(img.getchannel(0))
         
-        #centering
-        mean = pixels.mean()
-        pixels = pixels - mean
-
-        # normalize to the range 0-1
-        pixels = (pixels - pixels.min())/(pixels.max() - pixels.min())
-        print('after normalizing' + pixels)
-        # Save Image
-        PIL_img = Image.fromarray(np.uint8(pixels))
-        PIL_img.save('PP_Data/' + img_name)
-        newimg = asarray(PIL_img)
-        print('after conversion ' + newimg)
 
 start_time = time.time()
 #Kims Path: 'C:/Users/Monkk/OneDrive/Dokumenter/AAU/CS/CS2/01.P8/Data/ChinaSet_AllFiles/CXR_png'
 #Dennis Stationær: D:/Downloads/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png
+#Dennis laptop: C:/Users/Dennis/Downloads/xrayTB/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png'
 
-image_directory ='C:/Users/Dennis/Downloads/xrayTB/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png'
+img_dir ='D:/Downloads/ChinaSet_AllFiles/ChinaSet_AllFiles/CXR_png'
 
-preprocess(image_directory)
-label_files()
+# preprocess(image_directory)
+label_files(img_dir)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
