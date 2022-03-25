a050f16534576bc681a428d76ec1ffbfb0a83021
Thomas Vinther <dennistran0@gmail.com>
2022-03-25 09:44:47 +0100

    ll

--- src/Methods/Train.py
+++ src/Methods/Train.py
@@ -19,7 +19,7 @@ def KfoldTrain(net):
 
         num_epochs=2 # fra 10
         batch_size=64 #fra 128
-        k = 10 # dvs. hver fold er 10 %.
+        k = 10 # dvs. hver fold er 1/10.
         splits=KFold(n_splits=k,shuffle=True,random_state=42) #random state randomizer, men med det samme resultat. (seed)
         foldperf={}
 
