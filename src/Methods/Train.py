28a2fe6523bc57ec9b1681cfe378f3c791cf4bdb
Jakob <jrsa18@student.aau.dk>
2022-04-11 14:28:31 +0200

    changes
    changeschanges

--- src/Methods/Train.py
+++ src/Methods/Train.py
@@ -1,27 +1,39 @@
+from pickletools import float8
 from torchvision import datasets, transforms, models
 from sklearn.model_selection import KFold
 import torch
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.sampler import SubsetRandomSampler 
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter 
+from datetime import datetime
+import random
 
+
+            
 #kræver at nettet har self.model, self.criterion, self.optimizer. Evt. brug interface?
-def KfoldTrain(net):
+def KfoldTrain(net, model_name, checkpoint_every):
         transform_ting = transforms.Compose([
             transforms.Resize(net.input_size + 32), #fordi 224 + 32 = 256.
             transforms.CenterCrop(net.input_size),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-            
+
+                           
         data_dir = "../PP_data/" # husk opdelt i TP_positive, TP_positive
+        
+
         dataset = datasets.ImageFolder(data_dir,       
                         transform=transform_ting)
+
         #imagefolder konverterer vidst selv til RGB.
 
         torch.manual_seed(42)
+        random.seed(42)
+        
 
-        num_epochs=2 # fra 10
-        batch_size=8 #fra 128
+
+        batch_size=2 #fra 128
         k = 10 # dvs. hver fold er 1/10.
         splits=KFold(n_splits=k,shuffle=True,random_state=42) #random state randomizer, men med det samme resultat. (seed)
         foldperf={}
@@ -31,45 +43,139 @@ def KfoldTrain(net):
         #val_idx = ca. 10 % af træningssættet. Eks: [2, 13......]
         #og de storer bare indexer. 
         #og for hver fold skifter værdierne for train_idx og val_idx.
-        for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(dataset)))):
+
+        #variable to get the correct number of steps or image processed in tensorboard
+        total_steps = 0
+
+
+        num_train = len(dataset)
+        indices = list(range(num_train))
+        split = int(np.floor(len(dataset) * 0.8))
+        np.random.shuffle(indices)
+        test_idx, kfold_idx = indices[split:], indices[:split]
+        print(len(kfold_idx),len(test_idx))
+        KFold_sampler = SubsetRandomSampler(kfold_idx)
+        test_sampler = SubsetRandomSampler(test_idx)
+        #KFold_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=KFold_sampler)
+        test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=test_sampler)
+
+       #print(len(KFold_loader),len(test_loader))
+
+        for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(KFold_sampler)))):
 
             print('Fold {}'.format(fold + 1))
 
+            #Creates eventlog files for tensorboard, saved in runs map, names the file as the modelname/datetime
+            model_name_string = str(model_name)
+            now = datetime.now()
+            time_string = str(now.strftime("%d.%m.%Y.%H.%M.%S"))
+            writter = SummaryWriter("runs/{}_{}_{}".format(fold + 1,model_name_string,time_string))
+
             train_sampler = SubsetRandomSampler(train_idx)
-            test_sampler = SubsetRandomSampler(val_idx)
+            val_sampler = SubsetRandomSampler(val_idx)
             train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
-            test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=test_sampler)
+            val_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=val_sampler)
+
+      
             
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-            history = {'train_loss': [], 'test_loss': [],'train_acc':[],'test_acc':[]}
+            history = {'train_loss': [], 'val_loss': [],'train_acc':[],'val_acc':[]}
 
-            for epoch in range(num_epochs):
+             # Early stopping
+            last_loss = 100
+            patience = 2
+            trigger_times = 0
+            epoch = 0
+            
+            while epoch < 500:
+                epoch += 1 
+                
                 train_loss, train_correct=train_epoch(net.model,device,train_loader,net.criterion,net.optimizer, net.is_inception)
-                test_loss, test_correct=valid_epoch(net.model,device,test_loader,net.criterion, net.is_inception)
-
                 train_loss = train_loss / len(train_loader.sampler)
                 train_acc = train_correct / len(train_loader.sampler) * 100
-                test_loss = test_loss / len(test_loader.sampler)
-                test_acc = test_correct / len(test_loader.sampler) * 100
 
-                print("Epoch:{}/{} AVG Training Loss:{:.3f} AVG Test Loss:{:.3f} AVG Training Acc {:.2f} % AVG Test Acc {:.2f} %".format(epoch + 1,
-                                                                                                                    num_epochs,
-                                                                                                                    train_loss,
-                                                                                                                    test_loss,
-                                                                                                                    train_acc,
-                                                                                                                    test_acc))
-                history['train_loss'].append(train_loss)
-                history['test_loss'].append(test_loss)
-                history['train_acc'].append(train_acc)
-                history['test_acc'].append(test_acc)
+                #history['train_loss'].append(train_loss)            
+                #history['train_acc'].append(train_acc)
+                
+                print(len(train_loader.sampler))
+                total_steps += len(train_loader.sampler)
 
-            foldperf['fold{}'.format(fold+1)] = history  
+                #Her ændres hvor tit man vil tage checkpoint, 1 = hver gang
+                if epoch % checkpoint_every == 0:
+                    save_checkpoint(model_name, net, epoch)
+
+                    val_loss, val_correct=valid_epoch(net.model,device,val_loader,net.criterion, net.is_inception)               
+                    val_loss = val_loss / len(val_loader.sampler)
+                    val_acc = val_correct / len(val_loader.sampler) * 100 
+                    
+                    #writes to the SummaryWritten tensorboard object. 
+                    writter.add_scalar('TraniningLoss', train_loss, total_steps)
+                    writter.add_scalar('TraniningAccuracy', train_acc, total_steps)
+                    writter.add_scalar('ValLoss', val_loss, total_steps)
+                    writter.add_scalar('ValAccuracy', val_acc, total_steps)
+
+
+
+                    print("Epoch:{} AVG Training Loss:{:.8f} AVG Val Loss:{:.8f} AVG Training Acc {:.8f} % AVG Val Acc {:.8f} %".format(epoch,
+                                                                                                                        train_loss,
+                                                                                                                        val_loss,
+                                                                                                                        train_acc,
+                                                                                                                        val_acc))
+                    history['train_loss'].append(train_loss)
+                    history['val_loss'].append(val_loss)
+                    history['train_acc'].append(train_acc)
+                    history['val_acc'].append(val_acc)
+
+                    print('The Current Loss:', val_loss)
+
+                    early_stopping_val_loss = val_loss
+                    early_stopping_val_loss = format(early_stopping_val_loss, '.4f')
+                    early_stopping_last_loss = last_loss
+                    early_stopping_last_loss = format(early_stopping_last_loss, '.4f')
+    
+                    if early_stopping_val_loss > early_stopping_last_loss:
+                        trigger_times += 1
+                        print('Trigger Times:', trigger_times)
+    
+                        if trigger_times >= patience:
+                            epoch = 505
+                            print('Early stopping!\nStart to test process.')
+                            foldperf['fold{}'.format(fold+1)] = history
+
+                            test_loss, test_correct= test_method(net.model,device,test_loader,net.criterion, net.is_inception)
+                            test_correct = test_correct/ len(test_loader.sampler) * 100
+                            print(test_loss,test_correct)
+                            
+                    else:
+                        print('trigger times: 0')
+                        trigger_times = 0
+                    last_loss = val_loss
+                else:
+                    print("Epoch:{} AVG Training Loss:{:.8f} AVG Training Acc {:.8f} %".format(epoch,
+                                                                                            train_loss,                                                                                       
+                                                                                            train_acc))
+
+            #foldperf['fold{}'.format(fold+1)] = history  
+
+def save_checkpoint(model_name, net, epoch):
+        print("CHECKPOINT")
+        now = datetime.now()
+        epoch_string = str(epoch)
+        model_name_string = str(model_name)
+        time_string = str(now.strftime("%d.%m.%Y.%H.%M.%S"))
+        model_file_string = f"./checkpoints/{model_name_string}/model/modelcheckpoint-{epoch_string}-{model_name_string}-{time_string}.pth"
+        optimizer_file_string = f"./checkpoints/{model_name_string}/optimizer/optimizercheckpoint-{epoch_string}-{model_name_string}-{time_string}.pth"
+        model_FILE = model_file_string
+        optimizer_FILE = optimizer_file_string
+        torch.save(net.model.state_dict(), model_FILE)
+        torch.save(net.optimizer.state_dict(), optimizer_FILE)
 
 def train_epoch(model,device,dataloader,loss_fn,optimizer, is_inception):
         train_loss,train_correct=0.0,0
         model.train()
         for images, labels in dataloader:
+            
 
             images,labels = images.to(device),labels.to(device)
             optimizer.zero_grad()
@@ -107,3 +213,21 @@ def valid_epoch(model,device,dataloader,loss_fn, is_inception):
         val_correct+=(predictions == labels).sum().item()
 
     return valid_loss,val_correct
+
+
+def test_method(model,device,dataloader,loss_fn, is_inception):
+    test_loss, test_correct = 0.0, 0
+    model.eval()
+    for images, labels in dataloader:
+
+        images,labels = images.to(device),labels.to(device)
+        #lav
+
+        output = model(images)
+        loss = loss_fn(output,labels)
+
+        test_loss+=loss.item()*images.size(0)
+        scores, predictions = torch.max(output.data,1)
+        test_correct+=(predictions == labels).sum().item()
+
+    return test_loss,test_correct
--- src/Net.py
+++ src/Net.py
@@ -3,30 +3,51 @@ from torch import nn, relu
 from torch import optim
 from torchvision import models
 from Methods.Train import KfoldTrain
+import glob 
+import os
+
 
 #i høj grad inspireret fra https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html 
 
 class Net():
-    def __init__(self, model_name, pretrained = True, feature_extract = False, num_classes = 2): #features = tb positive/negative
+    def __init__(self, model_name, pretrained = True, feature_extract = False, num_classes = 2, checkpoint = False): #features = tb positive/negative
         self.model_path = './resnet.pth'
         self.data_dir = "../PP_data/" #skal være opdelt i TB_positive og TB_negative
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.is_inception = False
         self.input_size = 224
         print(self.device)
-        
         self.load_model(model_name, pretrained, num_classes)
+        self.load_checkpoint(checkpoint, model_name, "model")
+        
 
         self.model.eval()
         self.set_parameter_requires_grad(self.model, feature_extract, self.last_layer_name)
 
         #self.criterion = nn.NLLLoss() #virkede ikke med densenet. Spørg mig ikke hvorfor.
         self.criterion = nn.CrossEntropyLoss().cuda() if torch.cuda.is_available() else nn.CrossEntropyLoss()
-        self.set_optimizer(optim.Adam,feature_extract,self.last_layer_name, lr=0.003)
-        #self.optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        #self.set_optimizer(optim.Adam,feature_extract,self.last_layer_name, lr=0.003)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        self.load_checkpoint(checkpoint, model_name,  "optimizer")
+
+        #RMSprop optimizer function from efficientnet with paramters value from paper https://arxiv.org/pdf/1905.11946.pdf-.
+        #self.optimizer = self.optimizer = optim.RMSprop(self.model.parameters(), lr=0.01, momentum=0.9, alpha=0.853, eps=0.00422, weight_decay=9.714e-04)
         #self.print_parems_to_update(feature_extract)
         self.model.to(self.device)
 
+    def load_checkpoint(self, checkpoint, model_name, string):
+        #load model
+        if checkpoint and string == "model":
+            path_string = f"./checkpoints/{model_name}/model/*.pth"
+            list_of_files = glob.glob(f'{path_string}') # * means all if need specific format then *.csv
+            latest_file = max(list_of_files, key=os.path.getctime)
+            print("You have loaded the modelcheckpoint: " + latest_file)
+            FILE = f"{latest_file}"
+            self.model.load_state_dict(torch.load(FILE)) # it takes the loaded dictionary, not the path file itself
+        #load optimizer
+        if checkpoint and string == "optimizer":
+            path_string = f"./checkpoints/{model_name}/optimizer/*.pth"
+    
     def set_parameter_requires_grad(self, model, feature_extracting, last_layer_name):
         #altså hvis vi kun vil træne det sidste layer (classifier) så freezer vi alle andre layers.
         #dette kaldes også for feature_extracting.
@@ -73,7 +94,7 @@ class Net():
             self.last_layer_name = "classifier"
 
         elif model_name == model_names[1]: #resnet
-            self.model = models.resnet50(pretrained=pretrained)
+            self.model = models.resnext101_32x8d(pretrained=pretrained)
             num_features = self.model.fc.in_features
             self.model.fc = nn.Linear(num_features,num_classes)
             self.last_layer_name = "fc"
@@ -99,7 +120,7 @@ class Net():
             self.last_layer_name = "classifier"
         elif model_name == model_names[4]: #vgg19
             #self.model = models.vgg19(pretrained=pretrained)
-            self.model = models.vgg11_bn(pretrained=pretrained)
+            self.model = models.vgg19(pretrained=pretrained)
             print(self.model)
             self.model.classifier[6] = nn.Linear(in_features=4096, out_features=num_classes, bias=True)
             self.last_layer_name = "classifier"
@@ -109,6 +130,7 @@ class Net():
             #densenet har 1024 in_features hvilket jo skal angives i classifieren - num_features.
             self.model.classifier = nn.Linear(num_features,num_classes)
             self.last_layer_name = "classifier"
+
         elif model_name == model_names[6]: #inception v3
             self.is_inception = True
             self.model = models.inception_v3(pretrained=pretrained)
@@ -116,12 +138,22 @@ class Net():
             self.model.fc = nn.Linear(2048, num_classes)
             self.last_layer_name = "fc"
             self.input_size = 299
+
+        elif model_name == model_names[7]: #resnet
+            self.model = models.efficientnet_b7(pretrained=pretrained)
+            self.model.classifier = nn.Sequential(
+                nn.Dropout(p=0.5, inplace=True),
+                nn.Linear(in_features=2560, out_features=1000, bias=True),
+            )
+            self.last_layer_name = "classifier"
+            
+           
             
             
 
-model_names = ["densenet", "resnet", "squeezenet", "chexnet", "vgg", "densenet201", "inception"]
-net = Net(model_names[4])
-KfoldTrain(net)
+model_names = ["densenet", "resnext", "squeezenet", "chexnet", "vgg", "densenet201", "inception","efficientnet"]
+net = Net(model_names[0])
+KfoldTrain(net, model_names[0], checkpoint_every = 3)
 
 #husk grayscale ting!!
 #og husk det med at se på hvor sikker man er i sin prediction.
