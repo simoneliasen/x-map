157912bbe3bd9e4b8b96f855dd671efdaa90889c
Thomas Vinther <tvinther1@live.dk>
2022-04-27 12:42:59 +0200

    bedre parser

--- src/Methods/parser.py
+++ src/Methods/parser.py
@@ -5,8 +5,8 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     # Add an argument
     parser.add_argument('--data_path', type=str, required=False, default='../PP_data/')
-    parser.add_argument("--wandb", help="use wandb?", default=True, action='store_false')
-    parser.add_argument("--sweep", help="run sweep on hyperparameters for tuning", default=False, action='store_true')
+    parser.add_argument("--wandb", help="use wandb?", default=True, action=argparse.BooleanOptionalAction)
+    parser.add_argument("--sweep", help="run sweep on hyperparameters for tuning", default=False, action=argparse.BooleanOptionalAction)
     # Parse the argument
     args = parser.parse_args()
     return args
