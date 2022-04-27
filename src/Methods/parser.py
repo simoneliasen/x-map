5cb0a2195564d5a7eb592f694f8a681ce66586b3
Thomas Vinther <tvinther1@live.dk>
2022-04-27 14:02:54 +0200

    parser ændring til python 3.7

--- src/Methods/parser.py
+++ src/Methods/parser.py
@@ -5,7 +5,10 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     # Add an argument
     parser.add_argument('--data_path', type=str, required=False, default='../PP_data/')
-    parser.add_argument("--wandb", help="use wandb?", default=True, action=argparse.BooleanOptionalAction)
+    #parser.add_argument("--wandb", help="use wandb?", default=True, action=argparse.BooleanOptionalAction) #colab har ik python 3.9 :((((
+    parser.add_argument('--wandb', action='store_true')
+    parser.add_argument('--no-wandb', action='store_false')
+    parser.set_defaults(wandb=True)
     parser.add_argument("--sweep", help="run sweep on hyperparameters for tuning", default=False, action=argparse.BooleanOptionalAction)
     # Parse the argument
     args = parser.parse_args()
