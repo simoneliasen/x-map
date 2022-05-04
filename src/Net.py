8608077f5cf214c77a4ecf4646d3446c4e6ff4a7
Thomas Vinther <tvinther1@live.dk>
2022-05-04 13:18:34 +0200

    vgg test rms limit

--- src/Net.py
+++ src/Net.py
@@ -42,12 +42,12 @@ class Net():
             #sæt selv dine params her:
             #det er replicate af efficientnet, soft-sweep-1, der fik avg_val_acc på 96 %. Får den ca. det samme nu?
             custom_params = {
-                'batch_size': 64,
-                'dropout_rate': 0.08507472361218865,
-                'exponential_scheduler': 0.04477684052327043,
-                'lr': 0.06710935360616907,
-                'optimizer': 'sgd',
-                'weight_decay': 0.00006624338737215185,
+                'batch_size': 128,
+                'dropout_rate': 0.3767308917931543,
+                'exponential_scheduler': 0.027965205443403057,
+                'lr': 0.07354546106236301,
+                'optimizer': 'rmsprop',
+                'weight_decay': 0.00009905399059104637,
             }
             if args.batch_size is not None:
                 custom_params['batch_size'] = args.batch_size
