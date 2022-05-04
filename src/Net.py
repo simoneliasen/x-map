dc540b6cef319c317cad15f02ab5fc180966f43a
Thomas Vinther <tvinther1@live.dk>
2022-05-04 13:47:50 +0200

    Update Net.py

--- src/Net.py
+++ src/Net.py
@@ -56,6 +56,7 @@ class Net():
             print('custom hyperpameters!', params)
 
         if self.model_name == "densenet" and params['batch_size'] > 65:
+            print('nedsat batch size til 64')
             params['batch_size'] = 64
 
         #Og load params:
