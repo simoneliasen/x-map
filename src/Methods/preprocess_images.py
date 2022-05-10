4bfc9833dd5bd5f27df66a092f722c4f069e8430
Thomas Vinther <tvinther1@live.dk>
2022-05-10 17:51:00 +0200

    preprocess

--- src/Methods/preprocess_images.py
+++ src/Methods/preprocess_images.py
@@ -5,6 +5,7 @@ from os.path import isfile, join
 from PIL import Image
 import PIL
 import math
+import numpy as np
 
 imgpath = r"C:\Users\PC\Desktop\TB_Positve"
 #imgs = [f for f in listdir(imgpath) if isfile(join(imgpath, f))]
@@ -97,15 +98,15 @@ def add_padding(size, img):
     return black_mask
 
 def downscale(size):
-    pospath = r"C:\Users\PC\Desktop\nyt datasæt\PP_data\TB_Positive"
+    pospath = r"C:\Users\PC\Desktop\datasets\easy\PP_datav3\TB_Positive"
     #pospath = r"C:\Users\PC\Desktop\hejmor"
     pos = [f for f in listdir(pospath) if isfile(join(pospath, f))]
 
-    negpath = r"C:\Users\PC\Desktop\nyt datasæt\PP_data\TB_Negative"
+    negpath = r"C:\Users\PC\Desktop\datasets\easy\PP_datav3\TB_Negative"
     neg = [f for f in listdir(negpath) if isfile(join(negpath, f))]
 
-    newpospath = r"C:\Users\PC\Desktop\nyt datasæt\data_" + str(size) + "\TB_Positive"
-    newnegpath = r"C:\Users\PC\Desktop\nyt datasæt\data_" + str(size) + "\TB_Negative"
+    newpospath = r"C:\Users\PC\Desktop\datasets\easy\resized\TB_Positive"
+    newnegpath = r"C:\Users\PC\Desktop\datasets\easy\resized\TB_Negative"
 
     print(len(pos))
     print(len(neg))
@@ -191,5 +192,149 @@ def convert_mask2p():
 #for size in [299]:
     #downscale(size) #300 = inception
 
-downscale(299)
 
+def split_test_train():
+    pospath = r"C:\Users\PC\Desktop\datasets\easy\resized\TB_Positive"
+    pos = [f for f in listdir(pospath) if isfile(join(pospath, f))]
+
+    negpath = r"C:\Users\PC\Desktop\datasets\easy\resized\TB_Negative"
+    neg = [f for f in listdir(negpath) if isfile(join(negpath, f))]
+
+    new_pos_test_path = r"C:\Users\PC\Desktop\datasets\easy\test\TB_Positive"
+    new_pos_train_path = r"C:\Users\PC\Desktop\datasets\easy\train\TB_Positive"
+
+    new_neg_test_path = r"C:\Users\PC\Desktop\datasets\easy\test\TB_Negative"
+    new_neg_train_path = r"C:\Users\PC\Desktop\datasets\easy\train\TB_Negative"
+
+    print(len(pos))
+    print(len(neg))
+
+    #pos:
+    num_train = len(pos)
+    indices = list(range(num_train))
+    split = int(np.floor(num_train * 0.8))
+    np.random.shuffle(indices)
+    test_idx, train_idx = indices[split:], indices[:split]
+    
+    for index in test_idx:
+        file = pos[index]
+        path = pospath + f"\\{file}"
+        new_path = new_pos_test_path + f"\\{file}"
+        os.rename(path, new_path)
+
+    for index in train_idx:
+        file = pos[index]
+        path = pospath + f"\\{file}"
+        new_path = new_pos_train_path + f"\\{file}"
+        os.rename(path, new_path)
+        
+
+    #neg
+    num_train = len(neg)
+    indices = list(range(num_train))
+    split = int(np.floor(num_train * 0.8))
+    np.random.shuffle(indices)
+    test_idx, train_idx = indices[split:], indices[:split]
+    
+    for index in test_idx:
+        file = neg[index]
+        path = negpath + f"\\{file}"
+        new_path = new_neg_test_path + f"\\{file}"
+        os.rename(path, new_path)
+
+    for index in train_idx:
+        file = neg[index]
+        path = negpath + f"\\{file}"
+        new_path = new_neg_train_path + f"\\{file}"
+        os.rename(path, new_path)
+
+def remove_subset_from_dir():
+    remove_pos_path = r"C:\Users\PC\Desktop\datasets\hard\data_224\TB_Positive"
+    
+
+    remove_neg_path = r"C:\Users\PC\Desktop\datasets\hard\data_224\TB_Negative"
+    
+
+    new_neg_path = r"C:\Users\PC\Desktop\datasets\easy\resized - Kopi\TB_Negative"
+    new_neg = [f for f in listdir(new_neg_path) if isfile(join(new_neg_path, f))]
+    
+    new_pos_path = r"C:\Users\PC\Desktop\datasets\easy\resized - Kopi\TB_Positive"
+    new_pos = [f for f in listdir(new_pos_path) if isfile(join(new_pos_path, f))]
+
+    for file_name in new_pos:
+        try:
+            path = f"{remove_pos_path}\\{file_name}"
+            os.remove(path)
+
+        except Exception:
+            continue
+
+    for file_name in new_neg:
+        try:
+            path = f"{remove_neg_path}\\{file_name}"
+            os.remove(path)
+
+        except Exception:
+            continue
+
+def move_random_x_left():
+    #578 test filer fra easy
+    #864 skal i test (hard)
+
+    #dvs. der skal 864 - 578 = 286 ekstra findes fra covid osv.
+    #det er dog udelukkende negative
+
+    neg_path = r"C:\Users\PC\Desktop\datasets\hard\data_224\TB_Negative"
+    neg = [f for f in listdir(neg_path) if isfile(join(neg_path, f))]
+
+    new_neg_path = r"C:\Users\PC\Desktop\datasets\hard\test\TB_Negative"
+
+    num = len(neg)
+    indices = list(range(num))
+    split = 286
+    np.random.shuffle(indices)
+    test_idx = indices[:split]
+
+    for index in test_idx:
+        file = neg[index]
+        path = neg_path + f"\\{file}"
+        new_path = new_neg_path + f"\\{file}"
+        os.rename(path, new_path)
+
+def move_all_except_subset():
+    neg_path = r"C:\Users\PC\Desktop\datasets\hard\data_224 - Kopi - Kopi\TB_Negative"
+    neg = [f for f in listdir(neg_path) if isfile(join(neg_path, f))]
+    
+    pos_path = r"C:\Users\PC\Desktop\datasets\hard\data_224 - Kopi - Kopi\TB_Positive"
+    pos = [f for f in listdir(pos_path) if isfile(join(pos_path, f))]
+
+    new_neg_path = r"C:\Users\PC\Desktop\datasets\hard\train\TB_Negative"
+    new_pos_path = r"C:\Users\PC\Desktop\datasets\hard\train\TB_Positive"
+
+
+    test_neg_path = r"C:\Users\PC\Desktop\datasets\hard\test\TB_Negative"
+    test_neg = [f for f in listdir(test_neg_path) if isfile(join(test_neg_path, f))]
+    
+    test_pos_path = r"C:\Users\PC\Desktop\datasets\hard\test\TB_Positive"
+    test_pos = [f for f in listdir(test_pos_path) if isfile(join(test_pos_path, f))]
+
+    for file in neg:
+        if file not in test_neg:
+            path = neg_path + f"\\{file}"
+            new_path = new_neg_path + f"\\{file}"
+            os.rename(path, new_path)
+
+    for file in pos:
+        if file not in test_pos:
+            path = pos_path + f"\\{file}"
+            new_path = new_pos_path + f"\\{file}"
+            os.rename(path, new_path)
+#downscale(224)
+
+#split_test_train()
+
+#remove_files_from_dir()
+
+#move_random_x_left()
+
+move_all_except_subset()
